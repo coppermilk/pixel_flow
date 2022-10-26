8f7f570ee0a7dac123ee903f5ad5db78894635fb
Artem Herych <nataliyakowtun67@gmail.com>
2022-10-26 23:08:26 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/pixel_flow_run.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for pixel_flow_run.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zvgdb/iot/pixel_flow/Google_tests/lib/googletest
-# Build directory: /home/zvgdb/iot/pixel_flow/cmake-build-debug/Google_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
