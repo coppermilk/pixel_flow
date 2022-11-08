bb57082fdec5eefd5a290b8980ae122b4c9f28a1
Artem Herych <nataliyakowtun67@gmail.com>
2022-11-08 15:19:43 +0100

    reload repo

---
+++ cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/zvgdb/iot/4/pixel_flow/Google_tests/lib
+# Build directory: /home/zvgdb/iot/4/pixel_flow/cmake-build-debug/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zvgdb/iot/4/pixel_flow/Google_tests/lib/googletest
+# Build directory: /home/zvgdb/iot/4/pixel_flow/cmake-build-debug/Google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
