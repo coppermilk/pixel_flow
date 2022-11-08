8b37d198078bee8b694c4bbdd3804ed7664eb4fb
Artem Herych <nataliyakowtun67@gmail.com>
2022-11-08 16:08:03 +0100

    delete unused class

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,25 @@
 #include "GitHubDataExporter.h"
 #include "Data.h"
 #include <thread>
-//sudo apt install gcc
-// sudo apt install build-essential libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev
+
+// sudo apt install build-essential
 // sudo apt-get install python3-dev
+// sudo apt-get install libboost-all-dev
+// sudo apt install python3-pip
+// pip install requests
+// pip install bs4
+// pip install pandas
+
 int main()
 {
-	Data data;
+
 	
 
-	GitHubDataExporter::to_csv("coppermilk");
-	GoogleSheetsDataExporter::to_csv("1axA_i8GiCK1V7aQCcK4O5IPsjpGNsiAayjn7zziXpgA", "db");
-	GitHubDataExporter::to_csv("coppermilk");
-	data.update_data_from_csv("googlesheets.csv");
+	GitHubDataExporter::to_csv("coppermilk", "github.csv");
+	GoogleSheetsDataExporter::to_csv("1axA_i8GiCK1V7aQCcK4O5IPsjpGNsiAayjn7zziXpgA", "db", "googlesheets.csv");
 
+    Data data;
+	data.update_data_from_csv("googlesheets.csv");
 	data.update_data_from_csv("github.csv");
 	Board board(8, 8);
 	ControllerBoard controller_board(&board);
--- src/main.py
+++
@@ -1,7 +0,0 @@
-# The above code is reading the data from the google sheet and converting it into a csv file.
-import pandas as pd
-sheet_id = '1axA_i8GiCK1V7aQCcK4O5IPsjpGNsiAayjn7zziXpgA'
-sheet_name = 'db'
-url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
-df = pd.read_csv(url)
-df.to_csv('out.csv', encoding='ascii')
